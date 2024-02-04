462327697b273823302f85ee30db0b90f58ff6d7
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-02-04 22:34:57 +0530

    Multiple Components

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 //const heading =React.createElement("h1",{},"Hello React");
 //const heading=<h1 id="heading">Hello JSX</h1>;
-const heading =<h1>React Heading Component</h1>;
+const Heading =()=><h1>React Heading Component</h1>;
 
 const Div=() => (
     <div>
-    {heading}
+    {Heading()}
+    <Heading/>
+    <Heading></Heading>
     {100+300}
-    <p>React functional Component example</p>;
+    <p>React functional Component example</p>
     </div>
 );
 const root=ReactDOM.createRoot(document.getElementById("root"));
