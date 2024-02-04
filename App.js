3955a56c5b77567652b92015b58ca66abef10b14
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-02-04 22:10:15 +0530

    Using JSX

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-const heading =React.createElement("h1",{},"Hello React");
-//const heading=<h1 id="heading">Hello JSX</h1>;
+//const heading =React.createElement("h1",{},"Hello React");
+const heading=<h1 id="heading">Hello JSX</h1>;
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
