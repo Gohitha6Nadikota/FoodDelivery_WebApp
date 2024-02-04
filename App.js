55b948cf21f16298b70ee90c9f03166ff2364fbd
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-02-04 22:20:36 +0530

    Using Component Compositions

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 //const heading =React.createElement("h1",{},"Hello React");
-const heading=<h1 id="heading">Hello JSX</h1>;
+//const heading=<h1 id="heading">Hello JSX</h1>;
+const Heading = ( ) =>
+{
+    return <h1>React Heading Component</h1>;
+};
+
+const Div=() => (
+    <div>
+    <Heading/>
+    <p>React functional Component example</p>;
+    </div>
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Div/>);
