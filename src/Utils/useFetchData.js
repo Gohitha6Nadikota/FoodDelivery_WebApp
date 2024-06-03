f209d5ac0829927865493e7ba88202a151bf6560
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-06-03 16:59:11 +0530

    Modified path

--- src/Utils/useFetchData.js
+++ src/Utils/useFetchData.js
@@ -1,17 +1,19 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import { SWIGGY_API } from "../Utils/Constants";
-const useFetchData =()=>
-{
-    const [list,setList] = useState([]);
-    useEffect(()=>{
-        FetchData();
-    },[]);
+const useFetchData = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    FetchData();
+  }, []);
 
-    const FetchData = async ()=>{
-        const data = await fetch("http://localhost:1234/"+SWIGGY_API);
-        const json = await data.json();
-        setList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
-    return list;
-}
-export default useFetchData; 
+  const FetchData = async () => {
+    const data = await fetch("https://gh-food-delivery.vercel.app/" + SWIGGY_API);
+    const json = await data.json();
+    console.log(json.data);
+    setList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  return list;
+};
+export default useFetchData;
--- src/Utils/useRestaurantMenu.js
+++ src/Utils/useRestaurantMenu.js
@@ -1,17 +1,20 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { MENU_API } from "../Utils/Constants";
-const useRestaurantMenu =(resId)=>
-{
-    const [resInfo,setResInfo] = useState(null);
-    useEffect(()=>{
-        FetchData();
-    },[]);
-    const FetchData = async()=>
-    {
-        const data = await fetch("http://localhost:1234/" + MENU_API + resId);
-        const json= await data.json();
-        setResInfo(json.data);
-    }
-    return resInfo;
-}
+const useRestaurantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+  useEffect(() => {
+    FetchData();
+  }, []);
+  const FetchData = async () => {
+    const data = await fetch(
+      "https://gh-food-delivery.vercel.app/" +
+        MENU_API +
+        resId
+    );
+    const json = await data.json();
+    console.log(json.data);
+    setResInfo(json.data);
+  };
+  return resInfo;
+};
 export default useRestaurantMenu;
